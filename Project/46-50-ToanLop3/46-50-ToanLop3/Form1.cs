1f9a952677cfc18bf066251a705a3d7c520b0657
lequochao <lequochao@d7932fd2-5a84-7ae4-0fdd-74cb2935cf18>
2010-04-27 17:02:56 +0000

    Project của nhóm, các bạn có ý kiến gì khác thì cập nhật lên nha ! Sau khi phân công các nhóm sẽ tự tạo form của chương mình được phân công !

---
+++ Project/46-50-ToanLop3/46-50-ToanLop3/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _46_50_ToanLop3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập tên");
+                this.textBox1.Focus();
+            }
+            else
+            {
+                MucLuc frmMucLuc = new MucLuc();
+                this.Hide();
+                frmMucLuc.ShowDialog();
+                this.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

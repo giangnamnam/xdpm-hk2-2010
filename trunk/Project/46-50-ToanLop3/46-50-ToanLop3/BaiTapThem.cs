e9c8acfbc2b2363e3f96a51dae32d4b5f65eb66f
vanduoc0012 <vanduoc0012@d7932fd2-5a84-7ae4-0fdd-74cb2935cf18>
2010-04-28 14:56:38 +0000

    Thay đổi Menu

---
+++ trunk/Project/46-50-ToanLop3/46-50-ToanLop3/BaiTapThem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _46_50_ToanLop3
+{
+    public partial class BaiTapThem : Form
+    {
+        public BaiTapThem()
+        {
+            InitializeComponent();
+        }
+    }
+}

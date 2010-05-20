8b82bb2d9a6b50d7925f3141ee9e93e6cc2bf908
caocuong.0801 <caocuong.0801@d7932fd2-5a84-7ae4-0fdd-74cb2935cf18>
2010-05-20 00:05:39 +0000

    Đã Thêm Bài Tập Mới

---
+++ trunk/Project/46_47_48_49_50_ToanLop3(4)/46_47_48_49_50_ToanLop3/Phan5/BonPhepTinh100000.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace _46_47_48_49_50_ToanLop3.Phan5
+{
+    partial class BonPhepTinh100000
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bài 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BonPhepTinh100000
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 395);
+            this.Controls.Add(this.button1);
+            this.Name = "BonPhepTinh100000";
+            this.Text = "Bốn Phép Tính Trong Phạm Vi 100 000";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}

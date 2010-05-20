1c481389fdafa4d07e68959692f023f32782e42d
caocuong.0801 <caocuong.0801@d7932fd2-5a84-7ae4-0fdd-74cb2935cf18>
2010-05-20 09:25:59 +0000

    Có Lỗi Phát Sinh trong Phần 5. Đã sửa xong

---
+++ trunk/Project/46_47_48_49_50_ToanLop3(4)/46_47_48_49_50_ToanLop3/Phan5/CacSo100k.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace _46_47_48_49_50_ToanLop3.Phan5
+{
+    partial class CacSo100k
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btBai4 = new System.Windows.Forms.Button();
+            this.btBai3 = new System.Windows.Forms.Button();
+            this.btBai2 = new System.Windows.Forms.Button();
+            this.btBai1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(466, 421);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 19;
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Navy;
+            this.button4.Location = new System.Drawing.Point(431, 83);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 243);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Thoát";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Navy;
+            this.button3.Location = new System.Drawing.Point(30, 83);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 243);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Quay\r\nLại\r\n";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MistyRose;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.Location = new System.Drawing.Point(296, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 45);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Bài 4";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MistyRose;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(296, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 43);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Bài 3 b";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(154, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 26);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "CÁC SỐ ĐẾN 100 000";
+            // 
+            // btBai4
+            // 
+            this.btBai4.BackColor = System.Drawing.Color.MistyRose;
+            this.btBai4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btBai4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBai4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btBai4.Location = new System.Drawing.Point(296, 83);
+            this.btBai4.Name = "btBai4";
+            this.btBai4.Size = new System.Drawing.Size(110, 48);
+            this.btBai4.TabIndex = 13;
+            this.btBai4.Text = "Bài 3 a";
+            this.btBai4.UseVisualStyleBackColor = false;
+            this.btBai4.Click += new System.EventHandler(this.btBai4_Click_1);
+            // 
+            // btBai3
+            // 
+            this.btBai3.BackColor = System.Drawing.Color.MistyRose;
+            this.btBai3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btBai3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBai3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btBai3.Location = new System.Drawing.Point(159, 281);
+            this.btBai3.Name = "btBai3";
+            this.btBai3.Size = new System.Drawing.Size(103, 45);
+            this.btBai3.TabIndex = 12;
+            this.btBai3.Text = "Bai 2";
+            this.btBai3.UseVisualStyleBackColor = false;
+            this.btBai3.Click += new System.EventHandler(this.btBai3_Click_1);
+            // 
+            // btBai2
+            // 
+            this.btBai2.BackColor = System.Drawing.Color.MistyRose;
+            this.btBai2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btBai2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBai2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btBai2.Location = new System.Drawing.Point(159, 186);
+            this.btBai2.Name = "btBai2";
+            this.btBai2.Size = new System.Drawing.Size(103, 43);
+            this.btBai2.TabIndex = 11;
+            this.btBai2.Text = "Bài 1 b";
+            this.btBai2.UseVisualStyleBackColor = false;
+            this.btBai2.Click += new System.EventHandler(this.btBai2_Click_1);
+            // 
+            // btBai1
+            // 
+            this.btBai1.BackColor = System.Drawing.Color.MistyRose;
+            this.btBai1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btBai1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBai1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btBai1.Location = new System.Drawing.Point(159, 83);
+            this.btBai1.Name = "btBai1";
+            this.btBai1.Size = new System.Drawing.Size(103, 48);
+            this.btBai1.TabIndex = 10;
+            this.btBai1.Text = "Bài 1 a";
+            this.btBai1.UseVisualStyleBackColor = false;
+            this.btBai1.Click += new System.EventHandler(this.btBai1_Click_1);
+            // 
+            // CacSo100k
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 366);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btBai4);
+            this.Controls.Add(this.btBai3);
+            this.Controls.Add(this.btBai2);
+            this.Controls.Add(this.btBai1);
+            this.Name = "CacSo100k";
+            this.Text = "CacSo100k";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btBai4;
+        private System.Windows.Forms.Button btBai3;
+        private System.Windows.Forms.Button btBai2;
+        private System.Windows.Forms.Button btBai1;
+    }
+}

83fdfd3ddf34a0edf8b7a7fbb295ad4137853eab
caocuong.0801 <caocuong.0801@d7932fd2-5a84-7ae4-0fdd-74cb2935cf18>
2010-05-20 09:06:27 +0000

    Bổ sung thêm bài tập Phần 5

--- trunk/Project/46_47_48_49_50_ToanLop3(4)/46_47_48_49_50_ToanLop3/Phan5/BaiOnTap4/Bai04.cs
+++ trunk/Project/46_47_48_49_50_ToanLop3(4)/46_47_48_49_50_ToanLop3/Phan5/BaiOnTap4/Bai04.cs
@@ -15,5 +15,48 @@ namespace _46_47_48_49_50_ToanLop3.Phan5.BaiOnTap4
         {
             InitializeComponent();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Bạn muốn thoát chương trình", "Thoát", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Bai04_Load(object sender, EventArgs e)
+        {
+            radioButton1.Checked = radioButton2.Checked = radioButton3.Checked = radioButton4.Checked = false;
+            label6.Text = "";
+            label6.BackColor = Color.FromArgb(192,192,255);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = radioButton2.Checked = radioButton3.Checked = radioButton4.Checked = false;
+            label6.Text = "";
+            label6.BackColor = Color.FromArgb(192, 192, 255);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                label6.Text = "Chúc mừng bạn đã trả lời đúng câu hỏi!!!";
+                label6.BackColor = Color.FromArgb(255,255,128);
+
+            } 
+            else
+            {
+                label6.Text = "Tiếc quá!! Bạn trả lời sai rồi";
+                label6.BackColor = Color.FromArgb(255, 255, 128);
+            }
+        }
     }
 }

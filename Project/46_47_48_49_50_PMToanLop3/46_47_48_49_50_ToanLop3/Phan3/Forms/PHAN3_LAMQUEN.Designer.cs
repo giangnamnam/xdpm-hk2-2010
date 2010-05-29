1abab8b6eff586f6613f528abee4848d604a632a
huuvinh3010 <huuvinh3010@d7932fd2-5a84-7ae4-0fdd-74cb2935cf18>
2010-05-29 19:13:39 +0000

    Them Phan Ly Thuyet Lam quen so dem 10000_ Phan 3

--- Project/46_47_48_49_50_PMToanLop3/46_47_48_49_50_ToanLop3/Phan3/Forms/PHAN3_LAMQUEN.Designer.cs
+++ Project/46_47_48_49_50_PMToanLop3/46_47_48_49_50_ToanLop3/Phan3/Forms/PHAN3_LAMQUEN.Designer.cs
@@ -28,91 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox_Button = new System.Windows.Forms.GroupBox();
-            this.button_QuayLai = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.button_Sau = new System.Windows.Forms.Button();
+            this.button_Truoc = new System.Windows.Forms.Button();
             this.Title = new System.Windows.Forms.Label();
             this.tabControl_ = new System.Windows.Forms.TabControl();
-            this.tabPage_BaiTap = new System.Windows.Forms.TabPage();
             this.tabPage_LyThuyet = new System.Windows.Forms.TabPage();
-            this.groupBox_Button.SuspendLayout();
+            this.tabPage_BaiTap = new System.Windows.Forms.TabPage();
+            this.pictureBox_NoiDung = new System.Windows.Forms.PictureBox();
             this.tabControl_.SuspendLayout();
+            this.tabPage_LyThuyet.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_NoiDung)).BeginInit();
             this.SuspendLayout();
             // 
-            // groupBox_Button
+            // button_Sau
             // 
-            this.groupBox_Button.BackColor = System.Drawing.Color.Transparent;
-            this.groupBox_Button.Controls.Add(this.button_QuayLai);
-            this.groupBox_Button.Controls.Add(this.button4);
-            this.groupBox_Button.Controls.Add(this.button3);
-            this.groupBox_Button.Controls.Add(this.button2);
-            this.groupBox_Button.Controls.Add(this.button1);
-            this.groupBox_Button.Location = new System.Drawing.Point(648, 218);
-            this.groupBox_Button.Name = "groupBox_Button";
-            this.groupBox_Button.Size = new System.Drawing.Size(202, 343);
-            this.groupBox_Button.TabIndex = 9;
-            this.groupBox_Button.TabStop = false;
+            this.button_Sau.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_Sau.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Sau.Location = new System.Drawing.Point(273, 180);
+            this.button_Sau.Name = "button_Sau";
+            this.button_Sau.Size = new System.Drawing.Size(147, 45);
+            this.button_Sau.TabIndex = 3;
+            this.button_Sau.Text = " ";
+            this.button_Sau.UseVisualStyleBackColor = true;
+            this.button_Sau.Click += new System.EventHandler(this.button_Sau_Click);
             // 
-            // button_QuayLai
+            // button_Truoc
             // 
-            this.button_QuayLai.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button_QuayLai.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_QuayLai.Location = new System.Drawing.Point(26, 273);
-            this.button_QuayLai.Name = "button_QuayLai";
-            this.button_QuayLai.Size = new System.Drawing.Size(147, 45);
-            this.button_QuayLai.TabIndex = 7;
-            this.button_QuayLai.Text = "Quay &Lại";
-            this.button_QuayLai.UseVisualStyleBackColor = true;
-            this.button_QuayLai.MouseLeave += new System.EventHandler(this.button_QuayLai_MouseLeave);
-            this.button_QuayLai.Click += new System.EventHandler(this.button_Exit_Click);
-            this.button_QuayLai.MouseEnter += new System.EventHandler(this.button_Exit_MouseEnter);
-            // 
-            // button4
-            // 
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(26, 212);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(147, 45);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(26, 151);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(147, 45);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(26, 88);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(147, 45);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(26, 26);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(147, 45);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button_Truoc.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_Truoc.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Truoc.Location = new System.Drawing.Point(105, 180);
+            this.button_Truoc.Name = "button_Truoc";
+            this.button_Truoc.Size = new System.Drawing.Size(147, 45);
+            this.button_Truoc.TabIndex = 2;
+            this.button_Truoc.Text = " ";
+            this.button_Truoc.UseVisualStyleBackColor = true;
+            this.button_Truoc.Click += new System.EventHandler(this.button_Truoc_Click);
             // 
             // Title
             // 
@@ -120,7 +70,7 @@
             this.Title.BackColor = System.Drawing.Color.Transparent;
             this.Title.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Title.ForeColor = System.Drawing.Color.Red;
-            this.Title.Location = new System.Drawing.Point(349, 158);
+            this.Title.Location = new System.Drawing.Point(423, 9);
             this.Title.Name = "Title";
             this.Title.Size = new System.Drawing.Size(99, 31);
             this.Title.TabIndex = 8;
@@ -134,8 +84,23 @@
             this.tabControl_.Location = new System.Drawing.Point(83, 58);
             this.tabControl_.Name = "tabControl_";
             this.tabControl_.SelectedIndex = 0;
-            this.tabControl_.Size = new System.Drawing.Size(200, 100);
+            this.tabControl_.Size = new System.Drawing.Size(471, 293);
             this.tabControl_.TabIndex = 7;
+            // 
+            // tabPage_LyThuyet
+            // 
+            this.tabPage_LyThuyet.BackColor = System.Drawing.Color.White;
+            this.tabPage_LyThuyet.Controls.Add(this.pictureBox_NoiDung);
+            this.tabPage_LyThuyet.Controls.Add(this.button_Truoc);
+            this.tabPage_LyThuyet.Controls.Add(this.button_Sau);
+            this.tabPage_LyThuyet.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage_LyThuyet.ForeColor = System.Drawing.Color.Blue;
+            this.tabPage_LyThuyet.Location = new System.Drawing.Point(4, 31);
+            this.tabPage_LyThuyet.Name = "tabPage_LyThuyet";
+            this.tabPage_LyThuyet.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_LyThuyet.Size = new System.Drawing.Size(463, 258);
+            this.tabPage_LyThuyet.TabIndex = 0;
+            this.tabPage_LyThuyet.Text = "Lý Thuyết";
             // 
             // tabPage_BaiTap
             // 
@@ -143,21 +108,17 @@
             this.tabPage_BaiTap.Location = new System.Drawing.Point(4, 31);
             this.tabPage_BaiTap.Name = "tabPage_BaiTap";
             this.tabPage_BaiTap.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage_BaiTap.Size = new System.Drawing.Size(192, 65);
+            this.tabPage_BaiTap.Size = new System.Drawing.Size(463, 258);
             this.tabPage_BaiTap.TabIndex = 1;
             this.tabPage_BaiTap.Text = "Bài Tập";
             // 
-            // tabPage_LyThuyet
+            // pictureBox_NoiDung
             // 
-            this.tabPage_LyThuyet.BackColor = System.Drawing.Color.White;
-            this.tabPage_LyThuyet.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabPage_LyThuyet.ForeColor = System.Drawing.Color.Blue;
-            this.tabPage_LyThuyet.Location = new System.Drawing.Point(4, 31);
-            this.tabPage_LyThuyet.Name = "tabPage_LyThuyet";
-            this.tabPage_LyThuyet.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage_LyThuyet.Size = new System.Drawing.Size(192, 65);
-            this.tabPage_LyThuyet.TabIndex = 0;
-            this.tabPage_LyThuyet.Text = "Lý Thuyết";
+            this.pictureBox_NoiDung.Location = new System.Drawing.Point(105, 32);
+            this.pictureBox_NoiDung.Name = "pictureBox_NoiDung";
+            this.pictureBox_NoiDung.Size = new System.Drawing.Size(315, 120);
+            this.pictureBox_NoiDung.TabIndex = 4;
+            this.pictureBox_NoiDung.TabStop = false;
             // 
             // PHAN3_LAMQUEN
             // 
@@ -165,13 +126,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1108, 718);
             this.Controls.Add(this.tabControl_);
-            this.Controls.Add(this.groupBox_Button);
             this.Controls.Add(this.Title);
             this.Name = "PHAN3_LAMQUEN";
             this.Text = "PHAN3_LAMQUEN";
             this.Load += new System.EventHandler(this.PHAN3_LAMQUEN_Load);
-            this.groupBox_Button.ResumeLayout(false);
             this.tabControl_.ResumeLayout(false);
+            this.tabPage_LyThuyet.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_NoiDung)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -179,15 +140,12 @@
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox_Button;
-        private System.Windows.Forms.Button button_QuayLai;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_Sau;
+        private System.Windows.Forms.Button button_Truoc;
         private System.Windows.Forms.Label Title;
         private System.Windows.Forms.TabControl tabControl_;
         private System.Windows.Forms.TabPage tabPage_LyThuyet;
         private System.Windows.Forms.TabPage tabPage_BaiTap;
+        private System.Windows.Forms.PictureBox pictureBox_NoiDung;
     }
 }

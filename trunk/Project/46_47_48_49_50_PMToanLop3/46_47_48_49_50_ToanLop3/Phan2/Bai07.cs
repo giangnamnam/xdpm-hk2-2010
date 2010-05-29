3150e237230c85e16646704fe31bf7a00fa31f4f
vanduoc0012 <vanduoc0012@d7932fd2-5a84-7ae4-0fdd-74cb2935cf18>
2010-05-29 13:59:33 +0000

    Sắp Deadline anh em sao post ko cẩn thận dậy để sai hoài mà toàn lỗi bự commit lại lâu quá chừng đà này ko kịp đâu!!KT kỉ nhé anh em

---
+++ trunk/Project/46_47_48_49_50_PMToanLop3/46_47_48_49_50_ToanLop3/Phan2/Bai07.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _46_47_48_49_50_ToanLop3.Phan2
+{
+    public partial class Bai07 : Form
+    {
+        public Bai07()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuayLaiPhan2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Bạn muốn thoát chương trình", "Thoát", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Bai1.BaiTap7 frm = new Bai1.BaiTap7();
+            frm.ShowDialog();
+        }
+
+        private void llbThucHanh_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Bai1.LuyenTap.LuyenTapBT7 frm = new Bai1.LuyenTap.LuyenTapBT7();
+            frm.ShowDialog();
+        }
+    }
+}

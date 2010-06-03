1bee42d3f8f6a4bfd2ec703378f7151fb9f999a7
vanduoc0012 <vanduoc0012@d7932fd2-5a84-7ae4-0fdd-74cb2935cf18>
2010-06-03 07:49:31 +0000

    Hình ảnh cho các form của pm !!!

--- trunk/6 Source Code/46_47_48_49_50_ToanLop3/46_47_48_49_50_ToanLop3/PHAN5.Designer.cs
+++ trunk/6 Source Code/46_47_48_49_50_ToanLop3/46_47_48_49_50_ToanLop3/PHAN5.Designer.cs
@@ -35,6 +35,8 @@
             this.btDaiLuong = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -43,7 +45,7 @@
             this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(165, 91);
+            this.label1.Location = new System.Drawing.Point(529, 93);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(224, 26);
             this.label1.TabIndex = 1;
@@ -51,60 +53,107 @@
             // 
             // btCacSo100000
             // 
-            this.btCacSo100000.Location = new System.Drawing.Point(164, 138);
+            this.btCacSo100000.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btCacSo100000.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCacSo100000.ForeColor = System.Drawing.Color.Blue;
+            this.btCacSo100000.Location = new System.Drawing.Point(528, 140);
             this.btCacSo100000.Name = "btCacSo100000";
-            this.btCacSo100000.Size = new System.Drawing.Size(225, 34);
+            this.btCacSo100000.Size = new System.Drawing.Size(225, 57);
             this.btCacSo100000.TabIndex = 2;
             this.btCacSo100000.Text = "Các Số Đến 100 000\r\n";
-            this.btCacSo100000.UseVisualStyleBackColor = true;
+            this.btCacSo100000.UseVisualStyleBackColor = false;
             this.btCacSo100000.Click += new System.EventHandler(this.btCacSo100000_Click);
             // 
             // btCacSoDen100000tt
             // 
-            this.btCacSoDen100000tt.Location = new System.Drawing.Point(164, 191);
+            this.btCacSoDen100000tt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btCacSoDen100000tt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCacSoDen100000tt.ForeColor = System.Drawing.Color.Blue;
+            this.btCacSoDen100000tt.Location = new System.Drawing.Point(528, 203);
             this.btCacSoDen100000tt.Name = "btCacSoDen100000tt";
-            this.btCacSoDen100000tt.Size = new System.Drawing.Size(225, 34);
+            this.btCacSoDen100000tt.Size = new System.Drawing.Size(225, 59);
             this.btCacSoDen100000tt.TabIndex = 3;
             this.btCacSoDen100000tt.Text = "Các Số Đến 100 000 (tt)";
-            this.btCacSoDen100000tt.UseVisualStyleBackColor = true;
+            this.btCacSoDen100000tt.UseVisualStyleBackColor = false;
             this.btCacSoDen100000tt.Click += new System.EventHandler(this.btCacSoDen100000tt_Click);
             // 
             // btDaiLuong
             // 
-            this.btDaiLuong.Location = new System.Drawing.Point(164, 348);
+            this.btDaiLuong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btDaiLuong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDaiLuong.ForeColor = System.Drawing.Color.Blue;
+            this.btDaiLuong.Location = new System.Drawing.Point(528, 401);
             this.btDaiLuong.Name = "btDaiLuong";
-            this.btDaiLuong.Size = new System.Drawing.Size(225, 34);
+            this.btDaiLuong.Size = new System.Drawing.Size(225, 61);
             this.btDaiLuong.TabIndex = 4;
             this.btDaiLuong.Text = "Ôn Tập Hình Học";
-            this.btDaiLuong.UseVisualStyleBackColor = true;
+            this.btDaiLuong.UseVisualStyleBackColor = false;
             this.btDaiLuong.Click += new System.EventHandler(this.btDaiLuong_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(164, 243);
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Blue;
+            this.button4.Location = new System.Drawing.Point(528, 268);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(225, 34);
+            this.button4.Size = new System.Drawing.Size(225, 60);
             this.button4.TabIndex = 5;
             this.button4.Text = "Bốn Phép Toán Trong Pham Vi 100 000";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(164, 295);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(528, 334);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(225, 34);
+            this.button1.Size = new System.Drawing.Size(225, 61);
             this.button1.TabIndex = 6;
             this.button1.Text = "Bốn Phép Toán Trong Pham Vi 100 000 (tt)";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(864, 390);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 45);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "Quay Lại";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnThoat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.ForeColor = System.Drawing.Color.Black;
+            this.btnThoat.Location = new System.Drawing.Point(864, 440);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(141, 47);
+            this.btnThoat.TabIndex = 30;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
             // 
             // PHAN5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(499, 458);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1247, 549);
+            this.ControlBox = false;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.btDaiLuong);
@@ -112,7 +161,8 @@
             this.Controls.Add(this.btCacSo100000);
             this.Controls.Add(this.label1);
             this.Name = "PHAN5";
-            this.Text = "PHẦN 5";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.PHAN5_Load_1);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,5 +176,7 @@
         private System.Windows.Forms.Button btDaiLuong;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnThoat;
     }
 }

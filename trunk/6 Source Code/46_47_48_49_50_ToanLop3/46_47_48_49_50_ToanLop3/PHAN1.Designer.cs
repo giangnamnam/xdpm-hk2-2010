434bfb839119d4f94536a8abe5168b5dbbd1227c
vanduoc0012 <vanduoc0012@d7932fd2-5a84-7ae4-0fdd-74cb2935cf18>
2010-06-03 06:37:17 +0000

    Đã có Solution anh em kt có gì pm lại yahoo: vanduoc0012 nha. Chỉnh sửa form PHAN1

--- trunk/6 Source Code/46_47_48_49_50_ToanLop3/46_47_48_49_50_ToanLop3/PHAN1.Designer.cs
+++ trunk/6 Source Code/46_47_48_49_50_ToanLop3/46_47_48_49_50_ToanLop3/PHAN1.Designer.cs
@@ -43,12 +43,13 @@
             this.bài9ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.bài10ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.bai11ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btThoat
             // 
-            this.btThoat.Location = new System.Drawing.Point(794, 516);
+            this.btThoat.Location = new System.Drawing.Point(959, 706);
             this.btThoat.Name = "btThoat";
             this.btThoat.Size = new System.Drawing.Size(78, 34);
             this.btThoat.TabIndex = 0;
@@ -62,7 +63,7 @@
             this.mucLucToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(884, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1197, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -90,7 +91,7 @@
             // 
             this.bài1ToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
             this.bài1ToolStripMenuItem.Name = "bài1ToolStripMenuItem";
-            this.bài1ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bài1ToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.bài1ToolStripMenuItem.Text = "Bài 1";
             this.bài1ToolStripMenuItem.Click += new System.EventHandler(this.bài1ToolStripMenuItem_Click);
             // 
@@ -98,72 +99,82 @@
             // 
             this.bài2ToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
             this.bài2ToolStripMenuItem.Name = "bài2ToolStripMenuItem";
-            this.bài2ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bài2ToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.bài2ToolStripMenuItem.Text = "Bài 2";
             this.bài2ToolStripMenuItem.Click += new System.EventHandler(this.bài2ToolStripMenuItem_Click);
             // 
             // bai3ToolStripMenuItem
             // 
             this.bai3ToolStripMenuItem.Name = "bai3ToolStripMenuItem";
-            this.bai3ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bai3ToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.bai3ToolStripMenuItem.Text = "Bài 3";
             this.bai3ToolStripMenuItem.Click += new System.EventHandler(this.bai3ToolStripMenuItem_Click);
             // 
             // bài4ToolStripMenuItem
             // 
             this.bài4ToolStripMenuItem.Name = "bài4ToolStripMenuItem";
-            this.bài4ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bài4ToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.bài4ToolStripMenuItem.Text = "Bài 4";
             this.bài4ToolStripMenuItem.Click += new System.EventHandler(this.bài4ToolStripMenuItem_Click);
             // 
             // bài5ToolStripMenuItem
             // 
             this.bài5ToolStripMenuItem.Name = "bài5ToolStripMenuItem";
-            this.bài5ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bài5ToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.bài5ToolStripMenuItem.Text = "Bài 5";
             this.bài5ToolStripMenuItem.Click += new System.EventHandler(this.bài5ToolStripMenuItem_Click);
             // 
             // bài6ToolStripMenuItem
             // 
             this.bài6ToolStripMenuItem.Name = "bài6ToolStripMenuItem";
-            this.bài6ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bài6ToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.bài6ToolStripMenuItem.Text = "Bài 6";
             this.bài6ToolStripMenuItem.Click += new System.EventHandler(this.bài6ToolStripMenuItem_Click);
             // 
             // bài7ToolStripMenuItem
             // 
             this.bài7ToolStripMenuItem.Name = "bài7ToolStripMenuItem";
-            this.bài7ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bài7ToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.bài7ToolStripMenuItem.Text = "Bài 7 ";
             this.bài7ToolStripMenuItem.Click += new System.EventHandler(this.bài7ToolStripMenuItem_Click);
             // 
             // bai8ToolStripMenuItem
             // 
             this.bai8ToolStripMenuItem.Name = "bai8ToolStripMenuItem";
-            this.bai8ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bai8ToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.bai8ToolStripMenuItem.Text = "Bài 8";
             this.bai8ToolStripMenuItem.Click += new System.EventHandler(this.bai8ToolStripMenuItem_Click);
             // 
             // bài9ToolStripMenuItem
             // 
             this.bài9ToolStripMenuItem.Name = "bài9ToolStripMenuItem";
-            this.bài9ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bài9ToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.bài9ToolStripMenuItem.Text = "Bài 9";
             this.bài9ToolStripMenuItem.Click += new System.EventHandler(this.bài9ToolStripMenuItem_Click);
             // 
             // bài10ToolStripMenuItem
             // 
             this.bài10ToolStripMenuItem.Name = "bài10ToolStripMenuItem";
-            this.bài10ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bài10ToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.bài10ToolStripMenuItem.Text = "Bài 10";
             this.bài10ToolStripMenuItem.Click += new System.EventHandler(this.bài10ToolStripMenuItem_Click);
             // 
             // bai11ToolStripMenuItem
             // 
             this.bai11ToolStripMenuItem.Name = "bai11ToolStripMenuItem";
-            this.bai11ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bai11ToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.bai11ToolStripMenuItem.Text = "Bài 11";
             this.bai11ToolStripMenuItem.Click += new System.EventHandler(this.bai11ToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(875, 706);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Quay Lại";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // PHAN1
             // 
@@ -171,13 +182,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(884, 562);
+            this.ClientSize = new System.Drawing.Size(1197, 768);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btThoat);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "PHAN1";
-            this.Text = "PHẦN I";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.PHAN1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -202,5 +215,6 @@
         private System.Windows.Forms.ToolStripMenuItem bài9ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem bài10ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem bai11ToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
     }
 }

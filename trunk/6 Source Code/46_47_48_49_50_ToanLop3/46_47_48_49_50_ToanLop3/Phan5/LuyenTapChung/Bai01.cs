0d2ada78a021649d6082f1583b4b4eb1410c645b
vothanhcong2010 <vothanhcong2010@d7932fd2-5a84-7ae4-0fdd-74cb2935cf18>
2010-06-04 14:02:48 +0000

    Làm thêm phân Luyện Tập Chung trong phân 5

---
+++ trunk/6 Source Code/46_47_48_49_50_ToanLop3/46_47_48_49_50_ToanLop3/Phan5/LuyenTapChung/Bai01.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _46_47_48_49_50_ToanLop3.Phan5.LuyenTapChung
+{
+    public partial class Bai01 : Form
+    {
+        public Bai01()
+        {
+            InitializeComponent();
+        }
+
+        private void Bai01_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLXong_Click(object sender, EventArgs e)
+        {
+
+            if (txta.Text == "76245")
+            {
+                lbl1.Text = "Đúng";
+            }
+            else
+            {
+                lbl1.Text = "Sai";
+            }
+            if ((txtb.Text == "51807"))
+            {
+                lbl2.Text = "Đúng";
+            }
+            else
+            {
+                lbl2.Text = "Sai";
+            }
+            if ((txtc.Text == "90900"))
+            {
+                lbl3.Text = "Đúng";
+            }
+            else
+            {
+                lbl3.Text = "Sai";
+            }
+            if ((txtd.Text == "22002"))
+            {
+                lbl4.Text = "Đúng";
+            }
+            else
+            {
+                lbl4.Text = "Sai";
+            }
+
+        }
+
+        private void btnLLai_Click(object sender, EventArgs e)
+        {
+
+            txta.Text = txtb.Text = txtc.Text = txtd.Text = "";
+            lbl1.Text = lbl2.Text = lbl3.Text = lbl4.Text = "";
+        }
+
+        private void btnKQua_Click(object sender, EventArgs e)
+        {
+
+            lbl1.Text = "76245";
+            lbl2.Text = "51807";
+            lbl3.Text = "90900";
+            lbl4.Text = "22002";
+        }
+
+        private void btnQLai_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Bạn muốn thoát chương trình", "Thoát", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+       
+    }
+}
